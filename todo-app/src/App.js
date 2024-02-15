16a718f7e14d708003783a536f2ca9ae6b0187ee
MalinStal <malinstal.94@gmail.com>
2024-02-15 15:29:33 +0100

    added simpel todo logic to app.js

--- todo-app/src/App.js
+++ todo-app/src/App.js
@@ -1,24 +1,65 @@
-import logo from './logo.svg';
+import { useState, useEffect } from 'react';
 import './App.css';
 
 function App() {
+
+  const [todo, setTodo] = useState({title: "", description: ""});
+  const [list, setList] = useState([]);
+  
+const handelChange = (e) => {
+  e.preventDefault();
+  const { name, value } = e.target;
+ 
+  setTodo((prev) =>
+    (
+      {...prev, 
+        [name]: value
+      }
+    ))
+}
+
+  const handelSubmit = (e) => { 
+    e.preventDefault();
+    setList((prev) => [...prev,todo])
+    setTodo(
+    {title: "", description:""}
+   )
+   
+  }
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h1>Add todo</h1>
+      <form onSubmit={handelSubmit}>
+        <input
+          type='text'
+          name='title'
+          value={todo.title}
+          onChange={handelChange}
+        />
+        <input
+          type='text'
+          name='description'
+          value={todo.description}
+          onChange={handelChange}
+        />
+        <button>Add</button>
+      </form>
+      <ol>
+       {list.map((todo, index) => {
+      return (
+
+        <li key={index}>
+          <h3 >{todo.title}</h3>
+          <h5 >{todo.description}</h5>
+        </li>
+         )})
+        }
+      </ol>
+
+
+    </>
   );
 }
 
